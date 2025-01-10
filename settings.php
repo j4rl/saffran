86ea8651692fc8f47c8527316c1f45febd9cb24b
j4rl <charlie.jarl@gmail.com>
2025-01-10 09:53:31 +0100

    rfghjk

--- settings.php
+++ settings.php
@@ -2,8 +2,11 @@
 // Settings for language, theme and some corporate strings
 $language = "en";
 $theme = "default";
-$company = "My Company";
+$company = "Saffran";
 // get url of the company from the global variable $_SERVER
 $company_url = $_SERVER['HTTP_HOST'];
+//get logo url from /assets/logo.png and add the full path with help of $company_url
+$logo_url = "http://".$company_url."/assets/logo.png";
+
 
 ?>
